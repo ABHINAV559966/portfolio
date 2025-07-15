16fc612ce65cd986e805966ce0dfa0bf52b020d4
ABHINAV@559966 <abiadabalarajiadabala@gmail.com>
2025-07-15 15:05:11 +0530

    first

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-// script.js
-document.querySelector(".contact-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Message Sent! Thank you for reaching out.");
-});
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        menuToggle.checked = false; // Automatically close the menu
+      });
+    });
+  });
+
